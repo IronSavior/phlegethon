0dbeb5c150b15d2b0086dc4dec7731912d7808db
Erik Elmore <erik@ironsavior.net>
2013-04-26 14:36:18 -0400

    Workaround sleep_for bug in Boost 1.53
    Should be fixed in 1.54:
    https://svn.boost.org/trac/boost/ticket/8136
    
    closes #7

--- b_sleep_for.h
+++ b_sleep_for.h
@@ -2,13 +2,21 @@
 #define _GUARD_B_SLEEP_FOR_H_
 
 #include <chrono>
+#include <boost/chrono.hpp>
 #include <boost/thread.hpp>
 
 namespace boost { namespace this_thread {
+  // Allow boost::this_thread::sleep_for to accept std::chrono::duration
   template< class Rep, class Period >
   void sleep_for( const std::chrono::duration<Rep, Period>& d ) {
-    chrono::system_clock::duration _d(d.count());
-    sleep_for(_d);    
+    using chrono::steady_clock;
+    using chrono::duration;
+    using chrono::nanoseconds;
+    using chrono::ceil;
+    using _Period = ratio<Period::num, Period::den>;
+    auto _d = duration<Rep, _Period>(d.count());
+    auto wake_time = steady_clock::time_point(steady_clock::now() + ceil<nanoseconds>(_d));
+    sleep_until(wake_time);
   }
 }}
 
